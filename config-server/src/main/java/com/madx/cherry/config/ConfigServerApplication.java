d3b20c3cbf0f950885627cd40d5469171830afe4
A-mdx <634187415@qq.com>
2017-06-14 00:30:44 +0800

    这个分支可以完成了。

--- config-server/src/main/java/com/madx/cherry/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/madx/cherry/config/ConfigServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
